13570938c70bac0e4d8255555a886a3052e692fd
Mark Mims <mark.mims@gmail.com>
2021-10-17 16:25:44 -0400

    fix onprem slurm cluster to use new slurm-gcp version

--- terraform/onprem-slurm-cluster/main.tf
+++ terraform/onprem-slurm-cluster/main.tf
@@ -1,5 +1,5 @@
 #
-# Copyright 2020 Google LLC
+# Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,27 +22,29 @@ provider "google" {
   region  = local.region
 }
 
-#module "slurm_cluster_network" {
-  #source = "../../third_party/slurm-gcp/tf/modules/network"
+module "slurm_cluster_network" {
+  source = "../../third_party/slurm-gcp/tf/modules/network"
 
-  #cluster_name                  = var.cluster_name
-  #disable_login_public_ips      = var.disable_login_public_ips
-  #disable_controller_public_ips = var.disable_controller_public_ips
-  #disable_compute_public_ips    = var.disable_compute_public_ips
-  #network_name                  = var.network_name
-  #partitions                    = var.partitions
-  #shared_vpc_host_project       = var.shared_vpc_host_project
-  #subnetwork_name               = var.subnetwork_name
+  cluster_name                  = var.cluster_name
+  disable_login_public_ips      = var.disable_login_public_ips
+  disable_controller_public_ips = var.disable_controller_public_ips
+  disable_compute_public_ips    = var.disable_compute_public_ips
+  network_name                  = var.network_name
+  partitions                    = var.partitions
+  shared_vpc_host_project       = var.shared_vpc_host_project
+  subnetwork_name               = var.subnetwork_name
 
-  #project = var.project
-  #region  = local.region
-#}
+  project = var.project
+  region  = local.region
+}
 
 module "slurm_cluster_controller" {
   source = "../../third_party/slurm-gcp/tf/modules/controller"
 
   boot_disk_size                = var.controller_disk_size_gb
   boot_disk_type                = var.controller_disk_type
+  image                         = var.controller_image
+  instance_template             = var.controller_instance_template
   cluster_name                  = var.cluster_name
   compute_node_scopes           = var.compute_node_scopes
   compute_node_service_account  = var.compute_node_service_account
@@ -53,8 +55,8 @@ module "slurm_cluster_controller" {
   login_node_count              = var.login_node_count
   machine_type                  = var.controller_machine_type
   munge_key                     = var.munge_key
+  jwt_key                       = var.jwt_key
   network_storage               = var.network_storage
-  ompi_version                  = var.ompi_version
   partitions                    = var.partitions
   project                       = var.project
   region                        = local.region
@@ -62,10 +64,9 @@ module "slurm_cluster_controller" {
   secondary_disk_size           = var.controller_secondary_disk_size
   secondary_disk_type           = var.controller_secondary_disk_type
   shared_vpc_host_project       = var.shared_vpc_host_project
-  slurm_version                 = var.slurm_version
   scopes                        = var.controller_scopes
   service_account               = var.controller_service_account
-  #subnet_depend                 = module.slurm_cluster_network.subnet_depend
+  subnet_depend                 = module.slurm_cluster_network.subnet_depend
   subnetwork_name               = var.subnetwork_name
   suspend_time                  = var.suspend_time
   zone                          = var.zone
@@ -76,6 +77,8 @@ module "slurm_cluster_login" {
 
   boot_disk_size            = var.login_disk_size_gb
   boot_disk_type            = var.login_disk_type
+  image                     = var.login_image
+  instance_template         = var.login_instance_template
   cluster_name              = var.cluster_name
   controller_name           = module.slurm_cluster_controller.controller_node_name
   controller_secondary_disk = var.controller_secondary_disk
@@ -89,9 +92,8 @@ module "slurm_cluster_login" {
   service_account           = var.login_node_service_account
   munge_key                 = var.munge_key
   network_storage           = var.network_storage
-  ompi_version              = var.ompi_version
   shared_vpc_host_project   = var.shared_vpc_host_project
-  #subnet_depend             = module.slurm_cluster_network.subnet_depend
+  subnet_depend             = module.slurm_cluster_network.subnet_depend
   subnetwork_name           = var.subnetwork_name
   zone                      = var.zone
 }
@@ -100,10 +102,6 @@ module "slurm_cluster_compute" {
   source = "../../third_party/slurm-gcp/tf/modules/compute"
 
   cluster_name               = var.cluster_name
-  compute_image_disk_size_gb = var.compute_image_disk_size_gb
-  compute_image_disk_type    = var.compute_image_disk_type
-  compute_image_labels       = var.compute_image_labels
-  compute_image_machine_type = var.compute_image_machine_type
   controller_name            = module.slurm_cluster_controller.controller_node_name
   disable_compute_public_ips = var.disable_compute_public_ips
   network_storage            = var.network_storage
@@ -113,7 +111,7 @@ module "slurm_cluster_compute" {
   scopes                     = var.compute_node_scopes
   service_account            = var.compute_node_service_account
   shared_vpc_host_project    = var.shared_vpc_host_project
-  #subnet_depend              = module.slurm_cluster_network.subnet_depend
+  subnet_depend              = module.slurm_cluster_network.subnet_depend
   subnetwork_name            = var.subnetwork_name
   zone                       = var.zone
 }
